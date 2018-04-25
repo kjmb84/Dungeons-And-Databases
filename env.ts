3e32226df6e674c5b5a4cf7235b9dcd590d32efc
Kean Mattingly <kjmb84@mail.missouri.edu>
2018-04-25 07:49:15 -0500

    a

--- env.ts
+++ env.ts
@@ -1,12 +1,5 @@
 const environment = {
-    // mongoDB stuff
-    MongoUser: "admin",
-    MongoPassword: "IoNEMENDSpeNDlam",
-    MongoServer: "cluster0-bplqc.mongodb.net",
-    MongoDatabase: "test",
-    MongoScheme: "mongodb+srv:",
-
-    Port: 3000
+    
 };
 
 export = environment;
