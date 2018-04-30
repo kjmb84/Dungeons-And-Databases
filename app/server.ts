540c4d9eb260d8556ecbbc51df3124bc1d5e3d1e
Kean Mattingly <kjmb84@mail.missouri.edu>
2018-04-30 07:25:30 -0500

    start of work to allow cors

--- app/server.ts
+++ app/server.ts
@@ -17,6 +17,11 @@ mongoose.connect(mongoURL).catch(e => console.log(e));
  
 const app: express.Application = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 app.use(bodyParser.json());
 app.use('/api', ApiController);
 
