110745bb1e33d46ba2cc624c8c6d924f507f37e0
Kean Mattingly <kjmb84@mail.missouri.edu>
2018-05-23 12:54:49 -0500

    Removed old cors stuff

--- app/server.ts
+++ app/server.ts
@@ -4,35 +4,17 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import env from '../env';
 import NestedStructures from './nestedStructures';
-import * as cors from 'cors';
-const cors2 = require('cors');
+const cors = require('cors');
 
-const API_URL = '*';
-
-const options: cors.CorsOptions = {
-    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
-    credentials: true,
-    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-    origin: API_URL,
-    preflightContinue: false
-}
-
-// const mongoURL = `${env.MongoScheme}//${env.MongoUser}:${env.MongoPassword}@${env.MongoServer}/${env.MongoDatabase}`;
-const mongoURL = 'mongodb://localhost:27017/test';
-console.log(mongoURL);
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', () => {
-    console.log('connected');
-});
-mongoose.connect(mongoURL).catch(e => console.log(e));
+db.once('open', () => { console.log('connected'); });
+mongoose.connect(env.mongoUrl).catch(e => console.log(e));
  
 const app: express.Application = express();
 
-app.use(cors2());
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', ApiController);
 
-app.listen(env.Port, () => {
-    console.log("listening"); 
-});
+app.listen(env.Port, () => { console.log("listening"); });
--- env.ts
+++ env.ts
@@ -1,4 +1,5 @@
 const environment = {
+    mongoUrl: 'mongodb://localhost:27017/test',
     Port: 3000
 };
 
